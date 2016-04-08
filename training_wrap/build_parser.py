0de2001ec66864c9105ba0edaa8c9317ddd2db63
Chengxu Zhuang <zhuangcx93@gmail.com>
2016-04-08 18:05:10 -0400

    Build parser

--- training_wrap/build_parser.py
+++ training_wrap/build_parser.py
@@ -11,6 +11,7 @@ def build_parser():
         "--model_def",
         default='/om/user/chengxuz/sparse_CNN/alex_mine/train_val.prototxt_lmdb',
         help="Model definition file.")
-    parser.add_argument("--data_path", 
-        default='', 
-        help="")
+    parser.add_argument(
+        "--data_path",
+        default='/om/user/yamins/.skdata/imagenet/ChallengeSynsets2013_offline_23d6ee636ade8ad3912204410d1acc23c10357cf/cache/images_cache_e86d39462641ebc8870926aa16629eae5ca11c78_random_0_hdf5', 
+        help="Training data path")
